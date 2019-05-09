38d2a0970fe5dd3f3a9a147296e8cb480c9a5a2f
Catalin Sandu <sandu.catalin@earthlink.ro>
2019-05-09 11:39:16 +0300

    wip: beginnning

---
+++ chapter1/users.py
@@ -0,0 +1,23 @@
+#users list
+users = [
+    {"id": 0, "name": "Hero"},
+    {"id": 1, "name": "Dunn"},
+    {"id": 2, "name": "Sue"},
+    {"id": 3, "name": "Chi"},
+    {"id": 4, "name": "Thor"},
+    {"id": 5, "name": "Clive"},
+    {"id": 6, "name": "Hicks"},
+    {"id": 7, "name": "Devin"},
+    {"id": 8, "name": "Kate"},
+    {"id": 9, "name": "Klein"}
+]
+
+friendship_pairs = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
+                    (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
+
+friendships = { user['id']: [] for user in users }
+for i,j in friendship_pairs:
+    friendships[i].append(j)
+    friendships[j].append(i)
+
+print(friendships)
